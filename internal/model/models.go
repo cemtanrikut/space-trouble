24a619cbaf924bea90f5a38eb5ead60a20a1b6fa
Cem Tanrikut <cemtanrikut@gmail.com>
2024-10-30 19:00:40 +0100

    create booking model

---
+++ internal/model/models.go
@@ -0,0 +1,12 @@
+package model
+
+type Booking struct {
+	ID            int64
+	FirstName     string
+	LastName      string
+	Gender        string
+	Birthday      string
+	LaunchpadID   string
+	DestinationID string
+	LaunchDate    string
+}
