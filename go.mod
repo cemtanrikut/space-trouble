85b3d85fe22fa20b8420e59a56b34a1219b7b76d
Cem Tanrikut <cemtanrikut@gmail.com>
2024-10-30 18:57:43 +0100

    add postgres

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cmd/main.go
+
+go 1.22.1
+
+require github.com/lib/pq v1.10.9
--- pkg/db/postgres.go
+++ pkg/db/postgres.go
@@ -1,1 +1,19 @@
 package db
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq" // PostgreSQL driver
+)
+
+func NewPostgresDB(dataSourceName string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dataSourceName)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
